60c5901602bf28861bb0b6fb1f1b17e748fbab5d
LucasLimoenMan <140121885+ProfessorNether@users.noreply.github.com>
2023-10-12 12:15:59 +0200

    pong dual af

--- Assets/scripts/Ball_Script.cs
+++ Assets/scripts/Ball_Script.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
 
 public class Ball_Script : MonoBehaviour
 {
@@ -8,13 +10,35 @@ public class Ball_Script : MonoBehaviour
     public float Yposition = 0f;
     public float xSpeed;
     public float ySpeed;
+    public TMP_Text scoreField;
+    private int LeftScore = 0;
+    private int RightScore = 0;
+    public int topScore = 10;
+
+    private void resetBall (string leftOrRight)
+    {
+        Xposition = 0f; 
+        Yposition = 0f;
+        
+        scoreField.text = LeftScore + " - " + RightScore;
+        if(leftOrRight == "left")
+        {
+            xSpeed = 3f;
+            ySpeed = 3f;
+        }
+        else if (leftOrRight == "right") 
+        {
+            xSpeed = -3f;
+            ySpeed = 3f;
+        }
+    }
 
     // setting speed
     void Start()
     {
         transform.position= new Vector3(Xposition, Yposition, 0);
-        xSpeed = 5f;
-        ySpeed = 5f;
+        xSpeed = 3f;
+        ySpeed = 3f;
 
     }
 
@@ -25,6 +49,23 @@ public class Ball_Script : MonoBehaviour
         Yposition += ySpeed * Time.deltaTime;
 
         transform.position = new Vector3(Xposition, Yposition, 0);
+        if (LeftScore>= topScore)
+        {
+            scoreField.text = "Left player has won!";
+            xSpeed = 0f;
+            ySpeed = 0f;
+            Yposition= 0f;
+            Xposition = 0f;
+        }
+
+        else if (RightScore>=topScore)
+        {
+            scoreField.text = "Right player has won!";
+            xSpeed = 0f;
+            ySpeed = 0f;
+            Yposition = 0f;
+            Xposition = 0f;
+        }
 
     }
 
@@ -33,14 +74,27 @@ public class Ball_Script : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("HorizontalWall"))
         {
-            ySpeed = ySpeed * -1;
-            Debug.Log("Touched horizontal wall");
+            ySpeed = ySpeed * -1f;
+
         }
 
-        if (collision.gameObject.CompareTag("VerticalWall") || collision.gameObject.CompareTag("Stick"))
+        if (collision.gameObject.CompareTag("leftWall"))
         {
-            xSpeed = xSpeed * -1;
-            Debug.Log("Touched vertical wall");
+           
+            RightScore++;
+           resetBall("left");
         }
+        if (collision.gameObject.CompareTag("rightWall"))
+        {
+            
+            LeftScore++;
+            resetBall("right");
+
+        }
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            xSpeed = xSpeed * -1.1f;
+        }
+
     }
 }
