710771250138d06b7a218ed20722f2c5c977fb99
LucasLimoenMan <140121885+ProfessorNether@users.noreply.github.com>
2023-10-28 23:42:13 +0200

    Pong AI WERKT
    lees gewoon titel wollah schaap :)

--- Assets/scripts/Pong_AI.cs
+++ Assets/scripts/Pong_AI.cs
@@ -2,22 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//public class Pong_AI : MonoBehaviour
-//{
-//    public float ySpeed = 1f;
-//    public float yPosition = 0f;
+public class Pong_AI : MonoBehaviour
+{
+    public float ySpeed = 7f;
+    public float yPosition = 0;
+    public GameObject ball;
 
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-        
-//    }
+    // Start is called before the first frame update
+    void Start()
+    {
 
-//    // Update is called once per frame
-//    void Update()
-//    //{
-//    //    yPosition = yPosition + ySpeed * Time.deltaTime;
-//    //    transform.position = new Vector3 (transform.position.x, transform.yPosition/1.5f, transform.position.z);
-       
-//    //}
-//}
+    }
+
+    // Update is called once per frame
+    void Update()
+   {
+        yPosition = yPosition + ySpeed * Time.deltaTime;
+        transform.position = new Vector3 (transform.position.x, ball.transform.position.y/ ySpeed, transform.position.z);
+        transform.position = new Vector3(transform.position.x, ball.transform.position.y, transform.position.z);
+        if (yPosition >= 4)
+        {
+            ySpeed = ySpeed * -1f;
+        }
+        else if (yPosition <= -4)
+        {
+            ySpeed = ySpeed * -1f;
+        }
+
+    }
+}
