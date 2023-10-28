aa05aae2f6ce601d5155361304e484a3d28419d7
LucasLimoenMan <140121885+ProfessorNether@users.noreply.github.com>
2023-10-29 00:01:32 +0200

    AI FINAL

---
+++ Assets/scripts/Ball_Script_AI.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
+
+public class Ball_Script_AI : MonoBehaviour
+{
+    public float Xposition = 0f;
+    public float Yposition = 0f;
+    public float xSpeed;
+    public float ySpeed;
+    public TMP_Text scoreField;
+    private int LeftScore = 0;
+    private int RightScore = 0;
+    public int topScore = 10;
+
+    private void resetBall (string leftOrRight)
+    {
+        Xposition = 0f; 
+        Yposition = 0f;
+        
+        scoreField.text = LeftScore + " - " + RightScore;
+        if(leftOrRight == "left")
+        {
+            xSpeed = 3f;
+            ySpeed = 3f;
+        }
+        else if (leftOrRight == "right") 
+        {
+            xSpeed = -3f;
+            ySpeed = 3f;
+        }
+    }
+
+    // setting speed
+    void Start()
+    {
+        transform.position= new Vector3(Xposition, Yposition, 0);
+        xSpeed = 3f;
+        ySpeed = 3f;
+
+    }
+
+    // Speed Fix
+    void Update()
+    {
+        Xposition += xSpeed * Time.deltaTime;
+        Yposition += ySpeed * Time.deltaTime;
+
+        transform.position = new Vector3(Xposition, Yposition, 0);
+        if (LeftScore>= topScore)
+        {
+            scoreField.text = "HOW HAVE YOU WON?!?!!";
+            xSpeed = 0f;
+            ySpeed = 0f;
+            Yposition= 0f;
+            Xposition = 0f;
+        }
+
+        else if (RightScore>=topScore)
+        {
+            scoreField.text = "The Computer has won!";
+            xSpeed = 0f;
+            ySpeed = 0f;
+            Yposition = 0f;
+            Xposition = 0f;
+        }
+
+    }
+
+    //to do on impact
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("HorizontalWall"))
+        {
+            ySpeed = ySpeed * -1f;
+
+        }
+
+        if (collision.gameObject.CompareTag("leftWall"))
+        {
+           
+            RightScore++;
+           resetBall("left");
+        }
+        if (collision.gameObject.CompareTag("rightWall"))
+        {
+            
+            LeftScore++;
+            resetBall("right");
+
+        }
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            xSpeed = xSpeed * -1.1f;
+        }
+
+    }
+}
