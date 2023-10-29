d52a5f8eeadb425cd12e88b5c242e8ff33399945
LucasLimoenMan <140121885+ProfessorNether@users.noreply.github.com>
2023-10-29 02:32:10 +0200

    Somewhat done

--- Assets/scripts/Main_Menu.cs
+++ Assets/scripts/Main_Menu.cs
@@ -1,14 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Main_Menu : MonoBehaviour
 {
-    public void PlayGame()
+    public void PlayerVSPlayer()
     {
         SceneManager.LoadScene("2 Player Pong");
+    }
+
+    public void ComputerVSPlayer()
+    {
+        SceneManager.LoadScene("One Player Pong");
+    }
+
+    public void UltraCursedMode()
+    {
+        SceneManager.LoadScene("Ultra Cursed Pong");
+    }
+
+    public void GameInfo()
+    {
+        SceneManager.LoadScene("Game Info");
     }
     public void QuitGame()
     {
